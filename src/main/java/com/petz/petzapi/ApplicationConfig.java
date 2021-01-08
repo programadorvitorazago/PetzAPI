a21bb681dcceb20d9ecaaf0d9969fd6b91d086c4
programadorvitorazago <63272863+programadorvitorazago@users.noreply.github.com>
2021-01-08 01:51:00 -0300

    Update ApplicationConfig.java
    Correções finais

--- src/main/java/com/petz/petzapi/ApplicationConfig.java
+++ src/main/java/com/petz/petzapi/ApplicationConfig.java
@@ -5,31 +5,9 @@ import javax.ws.rs.core.Application;
 
 /**
  *
- * @author milan
+ * @author Vitor Zago
  */
 @javax.ws.rs.ApplicationPath("")
 public class ApplicationConfig extends Application {
-
     
-//    @Override
-//    public Set<Class<?>> getClasses() {
-//        Set<Class<?>> resources = new java.util.HashSet<>();
-//        addRestResourceClasses(resources);
-//        return resources;
-//    }
-//
-//    /**
-//     * Do not modify addRestResourceClasses() method.
-//     * It is automatically populated with
-//     * all resources defined in the project.
-//     * If required, comment out calling this method in getClasses().
-//     */
-//    private void addRestResourceClasses(Set<Class<?>> resources) {
-//        
-//        resources.add(entities.service.ClienteFacadeREST.class);
-//        resources.add(entities.service.CustomerFacadeREST.class);
-//        resources.add(entities.service.DiscountCodeFacadeREST.class);
-//        resources.add(entities.service.MicroMarketFacadeREST.class);
-//    }
-
 }
